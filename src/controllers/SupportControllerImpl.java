5a9745438476d57bf862ac3a75515f34ff063f11
Skylore <iturchin98@gmail.com>
2017-01-04 22:30:05 +0200

    removing support requests added

--- src/controllers/SupportControllerImpl.java
+++ src/controllers/SupportControllerImpl.java
@@ -22,7 +22,8 @@ public class SupportControllerImpl implements SupportController {
     @Override
     public void reply(SupportRequest request, String answer) throws NoSuchRequestException {
 
-        if (DataBase.getInstance().supportRequests.remove(request)) {
+        if (DataBase.getInstance().supportRequests.contains(request)) {
+            DataBase.getInstance().supportRequests.remove(request);
             SendMailSSL.sendLetter(request.getEmail(), "SendSay", answer);
         } else {
             throw new NoSuchRequestException();
